e0e3afda3af00c4f22d9508465e5c341e501e1bf
sofianeyra <safirokpoper@gmail.com>
2019-07-15 17:02:57 -0500

    prueba de estilos de BMD

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+      <div class="container">
+      <button type="button" class="btn btn-raised btn-primary">Primary</button>
+      <button type="button" class="btn btn-raised btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-raised btn-success">Success</button>
+      <button type="button" class="btn btn-raised btn-info">Info</button>
+      <button type="button" class="btn btn-raised btn-warning">Warning</button>
+      <button type="button" class="btn btn-raised btn-danger">Danger</button>
+      <button type="button" class="btn btn-raised btn-link">Link</button>
+      <button type="button" class="btn btn-raised active"><code>.active</code></button>
+    </div>
   </div>
 </template>
 
